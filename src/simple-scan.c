93b01e9e19c7aa5de158d359d61aea801a709917
robert.ancell@gmail.com <>
2009-11-17 23:55:47 -0600

    Added copyright headers

--- src/simple-scan.c
+++ src/simple-scan.c
@@ -1,3 +1,14 @@
+/*
+ * Copyright (C) 2009 Canonical Ltd.
+ * Author: Robert Ancell <robert.ancell@canonical.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version. See http://www.gnu.org/copyleft/gpl.html the full text of the
+ * license.
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
